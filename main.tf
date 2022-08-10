8f1862a0b7092578140d5975aede9546d56d83ce
chetan agrawal <chetanagrawalstar1999@gmail.com>
2022-08-10 13:30:49 +0530

    terraform module for ec2

---
+++ main.tf
@@ -0,0 +1,9 @@
+terraform {
+  
+}
+
+resource "aws_instance" "my_instance" {
+  ami           = var.ami_id 
+  instance_type = var.instance_type
+}
+
---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "ami_id" {
+  type    = string
+  default = "ami-090fa75af13c156b4"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
